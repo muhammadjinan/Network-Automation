26cac0d422bdc124871d10e5d4f780ece4ef54e2
Muhammad Jinan <muhammadjinanillikal@gmail.com>
2024-01-20 18:22:58 +0530

    Update populating empty list with for loop_3.py

--- Network Automation Labs/for and while loop/populating empty list with for loop_3.py
+++ Network Automation Labs/for and while loop/populating empty list with for loop_3.py
@@ -3,10 +3,10 @@ List2=[]                # Created a new empty list
 i=0                     # Created a variable 'i' and assigned value '0' to it
 for R in Branch_Router: # Assigned 'R' as the items of the list 'Branch_Router'
     List2.append(R)     #.append() is used to populate a list
-    print(f'List2 is populating with-> {i}',List2)
+    print(f'List2 is populating with-> {i}',List2[i])
     i+=1                # 1 is added to 'i' with the loop
-
+print(List2)
 # Run and see the difference with 
 #   "populating empty list with for loop_2.py"
 #   "populating empty list with for loop_3.py"
-    
+# Now replace 'List2[i]' with 'List2' (in line 6) and check the output    
