a433835aa60285ce921ae18df532401d11681f14
Muhammad Jinan <muhammadjinanillikal@gmail.com>
2024-01-20 21:14:43 +0530

    Added tuple
    'tuple' is a python collection (Array)
    collection of different data

---
+++ Network Automation Labs/tuple/tuple.py
@@ -0,0 +1,6 @@
+Branches=('KONDOTTY','KOCHI','DOHA','JEDDA','BANGLOR','CHENNAI','MADRAS')
+no=1
+print(type(Branches))       # Print the type of Array
+for item in Branches:           # Python collections are called "Array"
+    print(no,'-',item)
+    no+=1
