a0e2e102d433aca66b540b2677d0d4804f27197f
Muhammad Jinan <muhammadjinanillikal@gmail.com>
2024-01-20 21:24:59 +0530

    Update tuple.py
    added more info

--- Network Automation Labs/tuple/tuple.py
+++ Network Automation Labs/tuple/tuple.py
@@ -2,5 +2,5 @@ Branches=('KONDOTTY','KOCHI','DOHA','JEDDA','BANGLOR','CHENNAI','MADRAS')
 no=1
 print(type(Branches))       # Print the type of Array
 for item in Branches:           # Python collections are called "Array"
-    print(no,'-',item)
-    no+=1
+    print(no,'-',item+'.')  # '+' is used to concatenate the adjacent argument
+    no+=1                       # '+' removes space between two adjacent arguments while printing
