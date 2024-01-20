e15e6a1485c110d25ce471f2222df2e71b194bda
Muhammad Jinan <muhammadjinanillikal@gmail.com>
2024-01-20 21:55:36 +0530

    Update tuple.py
    added index printing

--- Network Automation Labs/tuple/tuple.py
+++ Network Automation Labs/tuple/tuple.py
@@ -1,5 +1,6 @@
 Branches=('KONDOTTY','KOCHI','DOHA','JEDDA','BANGLOR','CHENNAI','MADRAS')
 no=1
+print('HQ:',Branches[0])    # We can print items in a tuple with index 
 print(type(Branches))       # Print the type of Array
 for item in Branches:           # Python collections are called "Array"
     print(no,'-',item+'.')  # '+' is used to concatenate the adjacent argument
