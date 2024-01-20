bbb4df30eca625f32700961a4578b81978d2f6bd
Muhammad Jinan <muhammadjinanillikal@gmail.com>
2024-01-20 12:19:54 +0530

    Update AND and &.py
    command made more clear

--- Network Automation Labs/AND and &.py
+++ Network Automation Labs/AND and &.py
@@ -1,10 +1,10 @@
 a = 14
 b = 4
-#print(''.center(20,'*'))
-print(b and a)#"lazy evaluation" both are true and print last checked
-print(b & a)#14=1110 4=0100 so 1110 AND 0100=0100=>4
-#print(12 & 2)#12=1100 2=0010 so 1100 AND 0010=0000=>0
-#print(''.center(20,'*'))
+print(''.center(20,'*'))
+print(b and a)          #"lazy evaluation" both are true and print last checked
+print(b & a)            #14=1110 4=0100 so 1110 AND 0100=0100 | (0100 = 4)
+print(12 & 2)           #12=1100 2=0010 so 1100 AND 0010=0000 | (0000 = 0)
+print(''.center(20,'*'))
 
 if a<b and a>b:
     print("DONE")
