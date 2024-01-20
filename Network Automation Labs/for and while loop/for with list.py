6c3e58c89619631c941c2eb82ca5c9959c189811
Muhammad Jinan <muhammadjinanillikal@gmail.com>
2024-01-20 12:16:20 +0530

    updated with new files
    added for loop with list

---
+++ Network Automation Labs/for and while loop/for with list.py
@@ -0,0 +1,5 @@
+Branch_Router=['HSR','PUNE','GOA','ASAM','KERALA']
+for item in Branch_Router:
+    print(item)    #see the for loop and see the outout
+for item in range(len(Branch_Router)):
+    print(item)    # see the for loop and see the output
---
+++ Network Automation Labs/for and while loop/for with list_2.py
@@ -0,0 +1,4 @@
+Branch_Router=['HSR','PUNE','GOA','ASAM','KERALA']
+for item in range(len(Branch_Router)):
+    print(item,'-->',Branch_Router[item])
+    # Compare each arguments inside the print() with the output
