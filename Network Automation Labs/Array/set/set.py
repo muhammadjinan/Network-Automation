d01294d357bcce05c094272f9781685c3ac6843a
Muhammad Jinan <muhammadjinanillikal@gmail.com>
2024-01-21 09:54:09 +0530

    added set

---
+++ Network Automation Labs/Array/set/set.py
@@ -0,0 +1,14 @@
+Branches={'KONDOTTY','JEDDA','KOCHI','JEDDA'}
+# In this set we have two 'JEDDA' and let's see the output
+print(Branches)
+# Set consider True as 1 and 1 as True
+# Set consider False as 0 and 0 as False
+# See the command and the output to it
+Set1={True,'KOCHI','JEDDA'}
+print(Set1)
+Set2={1,'KOCHI','JEDDA'}
+print(Set2)
+Set3={True,'KOCHI','JEDDA',1}
+print(Set3)
+Set4={1,'KOCHI','JEDDA',True}
+print(Set4)
