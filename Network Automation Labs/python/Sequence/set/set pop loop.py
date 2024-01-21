36b9405531d7588d4990066db2ef47bf7820cf3d
Muhammad Jinan <muhammadjinanillikal@gmail.com>
2024-01-21 11:44:19 +0530

    Update set pop loop.py

--- Network Automation Labs/python/Sequence/set/set pop loop.py
+++ Network Automation Labs/python/Sequence/set/set pop loop.py
@@ -1,5 +1,7 @@
 Branches={'CHENNAI','JEDDA','KOCHI','DELHI','BENGALURU'}
-print('BEGINNING:',Branches)
+print('BEGINNING    :',Branches)
+print('Tracker of company status\n'+''.center(66,'-'))
 for i in range(len(Branches)):
-    Branches.pop()
-    print('THEN:     ',Branches)
+    ClosedBr=Branches.pop()
+    print('Closed Branch:',ClosedBr)
+    print('THEN         :',Branches)
