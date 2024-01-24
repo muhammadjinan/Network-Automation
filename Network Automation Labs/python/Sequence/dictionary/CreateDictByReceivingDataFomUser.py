9bafaa3199fdfc1372024012142a003fec6f0fa6
Muhammad Jinan <muhammadjinanillikal@gmail.com>
2024-01-24 08:03:45 +0530

    Update CreateDictByReceivingDataFomUser.py
    added more info

--- Network Automation Labs/python/Sequence/dictionary/CreateDictByReceivingDataFomUser.py
+++ Network Automation Labs/python/Sequence/dictionary/CreateDictByReceivingDataFomUser.py
@@ -2,7 +2,7 @@ Dic={}
 for _ in range(3):                    
     key = input('Enter the key')
     value = input('Enter the value')
-    Dic[key]=value
+    Dic[key]=value                    # We can also use 'Dic.update({key:value})'
 print(Dic)
 
 # If there is a pre-existing data then the new items will get added with the pre-existing
