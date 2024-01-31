f3a7b67ec28d4f4ccf803e1bf28da076ade7736c
Muhammad Jinan <muhammadjinanillikal@gmail.com>
2024-01-31 17:34:52 +0530

    Update netmikoGoToConfgMode.py

--- Network Automation Labs/pythonProject/netmiko/netmikoGoToConfgMode.py
+++ Network Automation Labs/pythonProject/netmiko/netmikoGoToConfgMode.py
@@ -6,13 +6,18 @@ print(promt)
 
 # In 'Netmiko' we need to enter certain commands to enter certain modes of Router
 
-# To enter 'User execution mode' (router#) if in 'Privileged mode' (router>)
+# Checking whether in 'Privileged mode' (router>) if in then go to
 if '>' in promt:
+    # To enter 'User execution mode' (router#) if in 'Privileged mode' (router>)
     connection.enable()
 
-# To enter 'Global configuration mode' (router(config)#) if not in the mode
+# Checking whether not in 'Global configuration mode' (router(config)#)
 if not connection.check_config_mode():
+    # To enter 'Global configuration mode' (router(config)#)
     connection.config_mode()
+
+# We can enter to certain mode even without checking, just enter the command to enter the certain mode
+# | connection.config_mode() | -> To enter 'Global configuration mode'
 
 # Print the current prompt
 print(connection.find_prompt())
